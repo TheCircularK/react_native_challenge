31f4faa66dbf1b54add8bed4998bbeb1a86d26f6
AJames <ajames1447@gmail.com>
2022-04-26 13:30:46 -0600

    Two functioning tabs. Functional clock.

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* Grab some modeules */
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; /* Get the navigation bar */
+import Clock from 'react-live-clock'; /* Clocks are nice */
+import {LineChart} from 'react-native-charts-wrapper'; /* So are graphs */
 
-export default function App() {
+/* Define the clock screen */
+function ClockScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'top', padding: 20, alignItems: 'center' }}>
+      <Text>Clock</Text>
+      <Clock format={'HH:mm:ss'} ticking={true} timezone={'US/Pacific'} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function GraphScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Graph</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function CreateTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Clock" component={ClockScreen} />
+      <Tab.Screen name="Graph" component={GraphScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <CreateTabs />
+    </NavigationContainer>
+  );
+}
---
+++ Screens/ClockScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function Home() {
+    return (
+        <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+            <Text>Clock Screen</Text>
+        </View>
+    )
+}
+
+export default Home
