04af2b3856808f33ce37ef88eb20e49ecd1ef8e8
AJames <ajames1447@gmail.com>
2022-04-26 23:55:08 -0600

    Added a graph tab. Next: data from API.

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 /* Grab some modeules */
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, SafeAreaView, StyleSheet, Dimensions, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; /* Get the navigation bar */
 import Clock from 'react-live-clock'; /* Clocks are nice */
-import {LineChart} from 'react-native-charts-wrapper'; /* So are graphs */
+/* So are graphs */
+import { LineChart } from 'react-native-chart-kit'
 
 /* Define the clock screen */
 function ClockScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'top', padding: 20, alignItems: 'center' }}>
-      <Text>Clock</Text>
+    <View style={ styles.titles }>
+      <Text style= {{ fontSize: 18 }}>Clock</Text>
       <Clock format={'HH:mm:ss'} ticking={true} timezone={'US/Pacific'} />
     </View>
   );
@@ -18,8 +19,35 @@ function ClockScreen() {
 
 function GraphScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={ styles.titles }>
       <Text>Graph</Text>
+        <LineChart
+          data={{
+            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+            datasets: [
+              {
+                data: [20, 45, 28, 80, 99, 43],
+                strokeWidth: 2,
+              },
+            ],
+          }}
+          width={Dimensions.get('window').width - 16}
+          height={220}
+          chartConfig={{
+            backgroundColor: '#1cc910',
+            backgroundGradientFrom: '#eff3ff',
+            backgroundGradientTo: '#efefef',
+            decimalPlaces: 2,
+            color: (opacity = 1) => 'rgba(0, 0, 0, .5)',
+            style: {
+              borderRadius: 16,
+            },
+          }}
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
     </View>
   );
 }
@@ -42,3 +70,13 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  titles: {
+    flex: 1,
+    fontSize: 50,
+    justifyContent: 'top',
+    padding: 20,
+    alignItems: 'center'
+  }
+})
